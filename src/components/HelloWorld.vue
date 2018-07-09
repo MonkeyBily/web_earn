1c92575fc8ebe7272e87a297e29c925ac096ad6f
MonkeyBily <40889027+MonkeyBily@users.noreply.github.com>
2018-07-09 21:40:56 +0800

    aa

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,350 @@
+<template>
+  <div class="global">
+    <div class="header" >
+      <h1 class="logo logonews"><a href="#xw.qq.com/m/news/index.htm">腾讯网</a></h1>
+      <span class="gochannels"></span> </div>
+    <div class="channels">
+      <ul style="width: 80%;height: 80%">
+        <li><a href="#xw.qq.com/">首页</a></li>
+        <li><a href="#xw.qq.com/m/news/">新闻</a></li>
+        <li><a href="#xw.qq.com/m/finance/">财经</a></li>
+        <li><a href="#xw.qq.com/m/ent/">娱乐</a></li>
+        <li><a href="#xw.qq.com/m/sports/">体育</a></li>
+        <li><a href="#xw.qq.com/m/stock/">证券</a></li>
+        <li><a href="#xw.qq.com/m/mil/">军事</a></li>
+        <li><a href="#xw.qq.com/m/tech/">科技</a></li>
+        <li><a href="#xw.qq.com/m/digi/">数码</a></li>
+        <li><a href="#xw.qq.com/m/fashion/">时尚</a></li>
+        <li><a href="http://w.auto.qq.com/?comefrom=xw">汽车</a></li>
+        <li><a href="#xw.qq.com/m/games/">游戏</a></li>
+        <li><a href="http://m.house.qq.com/index.php?mod=house&rf=newswap">房产</a></li>
+        <li><a href="#xw.qq.com/m/astro/">星座</a></li>
+        <li><a href="http://m.v.qq.com/">视频</a></li>
+      </ul>
+    </div>
+
+    <div class="primary">
+      <h1 class="title">高中生购买时时彩，竟盈利25万!</h1>
+      <div class="meta"> <span class="time" id="subtime">2017-10-26 10:02:55</span> <span class="author">人民日报客户端</span> <span class="contentbar"> <a href="#xw.qq.com/c/comments/1839380779/news1839380779"> <i class="count">1183</i> </a> <i class="size_s" id="fontsize">大字</i> </span> </div>
+      <div class="content fontsmall">
+        <p>近日，家住保定市徐水区的王女士发现正在上大学的儿子，银行卡里竟然有25万存款。在王女士和家人的逼问下孩子承认：今年3月份的时候，无意中看到一个微信号：123456，拉进群后，号称有专业研究团队根据走势给出购彩计划，于是便拿生活费跟着团队玩了一个多月时时彩赚了二十多万。本报记者为了检验事实也添加了这个微信号，竟在短短的几小时赚了六千多元，据了解这个群里大部分彩民都是跟着计划和师傅购彩。赔率竟然比传统彩票站赔率高了一倍。记者感慨到现在学生加入购彩行列，购彩低龄化趋势，学习被搁置，呼吁广大父母加强对自己孩子的监管。</p>
+        <p>记者：李宇</p>
+      </div>
+    </div>
+
+    <div class="share">
+      <dl>
+        <dt>分享到</dt>
+        <dd> <span class="share-wx-f"></span> <span class="share-wx-t"></span> <span class="share-qq"></span> <span class="share-qzone"></span> <span class="share-t"></span> <span class="share-wb"></span> </dd>
+      </dl>
+    </div>
+
+    <div class="comments">
+      <div class="head">
+        <h2>热门评论</h2>
+      </div>
+      <div class="content">
+        <ul class="commentlist">
+          <li>
+            <h3 class="meta"> Honest Heart
+              <time>昨天 11:47:47</time>
+            </h3>
+            <p class="content"> 太给力了，我也加了这个123456微信号，才一小时就挣了三百了！ </p>
+          </li>
+          <li>
+            <h3 class="meta"> 长龙时移
+              <time>昨天 11:21:36</time>
+            </h3>
+            <p class="content"> 是啊，挣钱真的太轻松了！</p>
+          </li>
+          <li>
+            <h3 class="meta"> Teenager
+              <time>前天 11:51:09</time>
+            </h3>
+            <p class="content"> 为我打开了新世界的大门</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="related">
+      <div class="head">
+        <h2>相关新闻</h2>
+      </div>
+      <div class="content">
+        <ul class="relatedlist">
+          <li class="full" data-id="NEW2017011701796101">
+            <a data-pos="related_2" data-id="NEW2017011701796101" class="app-item">
+              <div class="thumb">
+                <img src="../../static/641.jpg" alt="新iPhone三连发！iPhone X成史上最贵">
+              </div>
+              <h2>三代人守护古庙百年 老人患癌住院也要坚持</h2>
+              <p>
+                <span data-id="NEW2017011701796101" class="app-enter">下载应用查看</span>
+                <span class="source">中国新闻网</span>
+              </p>
+            </a>
+          </li>
+          <li class="full" data-id="NEW2017011701796101">
+            <a data-pos="related_2" data-id="NEW2017011701796101" class="app-item" >
+              <div class="thumb">
+                <img src="../../static/641.jpg" alt="新iPhone三连发！iPhone X成史上最贵">
+              </div>
+              <h2>谁说天下第一美人一定是女的？看看人家周渝民</h2>
+              <p>
+                <span data-id="NEW2017011701796101" class="app-enter">下载应用查看</span>
+                <span class="source">中国新闻网</span>
+              </p>
+            </a>
+          </li>
+          <li class="full" data-id="NEW2017011701796101">
+            <a data-pos="related_2" data-id="NEW2017011701796101" class="app-item">
+              <div class="thumb">
+                <img src="../../static/641.jpg" alt="新iPhone三连发！iPhone X成史上最贵">
+              </div>
+              <h2>辣眼睛！最丑的刘涛“霓凰郡主”蜡像曝光</h2>
+              <p>
+                <span data-id="NEW2017011701796101" class="app-enter">下载应用查看</span>
+                <span class="source">中国新闻网</span>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footnav">
+      <ul>
+        <li><a href="#xw.qq.com/">首页</a></li>
+        <li><a href="#xw.qq.com/m/news/">新闻</a></li>
+        <li><a href="#xw.qq.com/m/finance/">财经</a></li>
+        <li><a href="#xw.qq.com/m/ent/">娱乐</a></li>
+        <li><a href="#xw.qq.com/m/sports/">体育</a></li>
+        <li><a href="#xw.qq.com/m/stock/">证券</a></li>
+        <li><a href="#xw.qq.com/m/mil/">军事</a></li>
+        <li><a href="#xw.qq.com/m/tech/">科技</a></li>
+        <li><a href="#xw.qq.com/m/digi/">数码</a></li>
+        <li><a href="#xw.qq.com/m/fashion/">时尚</a></li>
+        <li><a href="http://w.auto.qq.com/?comefrom=xw">汽车</a></li>
+        <li><a href="#xw.qq.com/m/games/">游戏</a></li>
+        <li><a href="http://m.house.qq.com/index.php?mod=house&rf=newswap">房产</a></li>
+        <li><a href="#xw.qq.com/m/astro/">星座</a></li>
+        <li><a href="http://m.v.qq.com/">视频</a></li>
+      </ul>
+    </div>
+    <div class="footer">
+      <div class="links"> <a href="#url.cn/JVoGlc" class="repweb">反馈</a> <a href="#news.qq.com/zt2014/2014qtnews/ccybspxd.htm">举报中心</a> <a href="javascript:void(0)" class="pcs">电脑版</a> </div>
+      <div class="copyright"> Copyright &copy; 1998 - 2017 Tencent. 腾讯科技（北京）有限公司 All Rights Reserved </div>
+      <div class="gotop"></div>
+    </div>
+    <div class="btcodea tankuang">
+        <span style="white-space: normal; text-align:center; position: relative;">
+            <img class="weixinicon" src="../../static/weixin.png" alt="" srcset="">
+            <strong>
+                <span style=" font-size: 14px;">点击加入我们的团队</span>
+            </strong>
+        </span>
+      <strong id="wechat12" style="padding: 5px 10px;background: #f5fb01;border-radius: 6px;color: #d32a63;">
+            <span style="max-width: 100%; color: rgb(255, 41, 65); box-sizing: border-box !important; word-wrap: break-word !important; background-color: rgb(255, 251, 0);">
+                <b>
+                    <a class="con" oncopy="javascript:window.location.href='weixin://'">
+                        <b v-on:click="open()" style="cursor: pointer">123456</b>
+                    </a>
+                </b>
+            </span>
+      </strong>
+    </div>
+    <div id="tankuang" v-if="seen">
+    <div class="wechat" id="wechat" style="display: block; ">
+      <a class="screen-name" style="color: #777;">
+      </a><p class="p1"><a class="screen-name" style="color: #777;">
+      <span class="msg">点击复制，打开微信号添加好友</span>
+    </a><a class="closeBtn" v-on:click="close()" style="cursor:pointer">X</a>
+    </p>
+      <p class="wxid">
+        <input type="text" id="wechatStr" :value="copyValue">
+        <button id="wechatBtn"  v-on:click.stop="copy()" >点我复制</button>
+      </p>
+      <p class="p2">
+        <a href="weixin://" onclick="PIWI_SUBMIT.Weixin_Open()">
+          打开微信
+        </a>
+      </p>
+    </div>
+    </div>
+  </div>
+</template>
+<style>
+  .btcodea {
+    width: 100%;
+    background: #2680d8;
+    color: #FFF;
+    font-size: 0.9em;
+    line-height: 3em;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    -webkit-transform: translate(-50%, 0);
+  }
+
+  .btcodea .weixinicon {
+    position: absolute;
+    right: 100%;
+    top:-13px;
+  }
+
+  #tankuang {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, .4);
+  }
+
+  #tankuang.hide {
+    display: none;
+  }
+
+  .wechat .wxid {
+    margin: 0;
+  }
+
+  #tankuang>.wechat {
+    position: fixed;
+    background: white;
+    text-align: center;
+    width: 80%;
+    border: 1px solid #ccc;
+    border-radius: 12px;
+    box-shadow: 10px 10px 10px #ccc;
+    -webkit-box-shadow: 10px 10px 10px #ccc;
+    overflow: hidden;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    padding: 0;
+
+  }
+
+  #tankuang>.wechat .p1 {
+    font-size: 28px;
+    font-weight: bold;
+    padding: 10px;
+  }
+
+  #tankuang>.wechat .wxid {
+    font-size: 30px;
+    background: #277edb;
+    color: white;
+    padding: 10px;
+  }
+
+  #tankuang>.wechat .p2 {
+    font-size: 28px;
+    padding: 10px;
+  }
+
+  #tankuang>.wechat .p2:active {
+    background: red;
+  }
+
+  #tankuang>.wechat .p2:active a {
+    color: white;
+  }
+
+  .screen-name {
+    position: relative;
+  }
+
+  .closeBtn {
+    color: red;
+    display: block;
+    font-size: 40px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 0 20px;
+    border-radius: 20%;
+    height: 100%;
+    line-height: 1.2;
+  }
+
+  .closeBtn:hover {
+    background: red;
+    color: white;
+  }
+
+  #wechatBtn {
+    border: 1px solid #a7a7a7;
+    border-radius: 8px;
+    background: white;
+    padding: 8px 24px;
+    position: relative;
+    top: -6px;
+    overflow: hidden;
+  }
+
+  #wechatBtn:hover {
+    background: #ccc;
+
+  }
+
+  #wechatStr {
+    font-size: 30px;
+    color: white;
+    height: 45px;
+    width: 215px;
+    text-align: center;
+    border: none;
+    display: inline-block;
+    background: none;
+    overflow: hidden;
+  }
+
+  .p1 {
+    position: relative;
+  }
+
+  @media screen and (max-width: 321px) {
+    .screen-name {
+      font-size: 20px;
+    }
+  }
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+export default
+{name: 'HelloWorld',
+  data () {
+    return {
+      seen: false,
+      copyValue: '12345161'
+    }
+  },
+  methods: {
+    close: function () {
+      this.seen = false
+    },
+    open: function () {
+      this.seen = true
+    },
+    copy: function () {
+      // const btn = document.querySelector('#wechatBtn')
+      const input = document.querySelector('#wechatStr')
+      input.select()
+      if (document.execCommand('copy')) {
+        // document.execCommand('copy');
+        var comf = window.confirm('复制成功,是否打开微信?')
+      } else {
+        alert('复制失败,请长按复制')
+      }
+      if (comf) {
+        window.open('weixin://')
+      }
+    }
+  }
+}
+</script>
